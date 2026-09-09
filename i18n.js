8dfc6559a01319b91a0a4e9aa33112160a1e0af8
romanvereta-create <romanvereta@gmail.com>
2026-09-09 11:51:14 +0300

    Add files via upload

--- i18n.js
+++ i18n.js
@@ -1,5 +1,5 @@
 (() => {
-    const VERSION = '31.1.1-rc1';
+    const VERSION = '31.1.2-rc1';
     const LANGUAGES = {
         ru: { label: 'Русский', locale: 'ru-RU', currency: 'RUB' },
         en: { label: 'English', locale: 'en-US', currency: 'USD' },
